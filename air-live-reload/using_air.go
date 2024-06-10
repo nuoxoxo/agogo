6a6326ba190f96ba0c595eff51ea8f9313a9af8b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-06-10 19:49:31 +0200

    air/air init - why must use kill dash 9

--- air-live-reload/using_air.go
+++ air-live-reload/using_air.go
@@ -16,11 +16,25 @@ func main() {
 func handle_index (w http.ResponseWriter, r * http.Request) {
 
   ending := " \n"
+  // var N int
+  var msg string
 
-  log.Println("method/ ", r.Method, ending)
-  log.Println("URL   / ", r.URL, ending)
-  log.Println("header/ ", r.Header, ending)
+  log.Println("method/", r.Method, ending)
+  log.Println("URL   /", r.URL, ending)
+  for key, val := range r.Header {
+    if len(val) != 1 {
+      print(key, " - ", val)
+    }
+    if len(val[0]) < 42 {
+      msg = val[0]
+    } else {
+      msg = val[0][:42]
+      msg += " ... "
+    }
+    log.Println("header/", key, msg)
+  }
+  // log.Println("header/ ", r.Header, ending)
 
-  w.Write( []byte("hello world") )
+  w.Write([]byte("hello world"))
 }
 
